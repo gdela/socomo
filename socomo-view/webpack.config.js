29285911556c213286ccf0057d2b8f3905a14fbe
Wojciech Gdela <wojciech@gdela.pl>
2019-04-06 03:18:41 +0200

    Added babel polyfills.

--- socomo-view/webpack.config.js
+++ socomo-view/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: ['@babel/preset-env']
+							presets: [['@babel/preset-env', { useBuiltIns: "usage", corejs: 2 }]]
 						}
 					},
 					{
